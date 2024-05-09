f3484e935108b96c84fd7d87aed0a7af9f3fe230
Md Shihabul Islam <mdshihabulislam.mte.ruet@gmail.com>
2024-05-09 20:16:06 +0600

    dont know

--- frontend/src/app/rider/page.tsx
+++ frontend/src/app/rider/page.tsx
@@ -5,7 +5,7 @@ const Rider = () => {
   return (
     <div className="px-64 py-48">
       <RideCard
-        location="talaimari"
+        location="Talaimari"
         destination="Bazar"
         fare="100"
         date="2021-09-01"
