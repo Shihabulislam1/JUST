14e50cc8584d7a6d662d787c577d2fbaa595b60c
Md Shihabul Islam <mdshihabulislam.mte.ruet@gmail.com>
2024-05-09 20:16:57 +0600

    err

--- frontend/src/app/rider/page.tsx
+++ frontend/src/app/rider/page.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import RideCard from "../../components/RideCard/RideCard";
 
 const Rider = () => {
+
+
   return (
-    <div className="px-64 py-48">
+    <div className="px-64 py-48 ">
       <RideCard
         location="Talaimari"
         destination="Bazar"
