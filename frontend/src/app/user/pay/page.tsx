3ee6a234df45d3f1e2b1ed60ba2d87fe5e741c6c
Md Shihabul Islam <mdshihabulislam.mte.ruet@gmail.com>
2024-05-09 21:18:45 +0600

    err fixed

--- frontend/src/app/user/pay/page.tsx
+++ frontend/src/app/user/pay/page.tsx
@@ -1,12 +1,15 @@
-import PayNow from '@/components/PayNow/PayNow'
-import React from 'react'
+import PayNow from "@/components/PayNow/PayNow";
+import React from "react";
 
 const Pay = () => {
   return (
-    <div className='max-w-[1200px] m-auto mt-32'>
-      <PayNow/>
+    <div className="max-w-[1200px] m-auto mt-32">
+      <PayNow
+        fare={450}
+        riderAccount="0x21Fd62F8b34D174f536c8239543Bf895f3ACAbb9"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Pay
+export default Pay;
